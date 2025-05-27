5296d6ef993f3ba93f21e453fbebc6868872b8fa
Goutham Kumar <root@ip-172-31-85-159.ec2.internal>
2025-05-27 02:19:58 +0000

    initial commit for 3 tier app in terraform

---
+++ variables.tf
@@ -0,0 +1,21 @@
+# Networking (already existing in your account)
+variable "vpc_id"                   { type = string }
+variable "public_subnet_ids"        { type = list(string) }   # ≥2 AZs
+variable "private_app_subnet_ids"   { type = list(string) }   # ≥2 AZs
+variable "private_db_subnet_ids"    { type = list(string) }   # ≥2 AZs
+
+# CIDRs for SG source rules
+variable "public_subnet_cidrs"      { type = list(string) }
+variable "private_app_subnet_cidrs" { type = list(string) }
+
+# Misc
+variable "project_name" { type = string default = "tier3app" }
+variable "key_name"     { type = string }                # existing EC2 KeyPair
+variable "app_port"     { type = number  default = 3000 }
+variable "hosted_zone_id" { type = string }              # public hosted zone
+variable "domain_name"    { type = string }              # e.g. app.example.com
+
+# RDS
+variable "db_username" { type = string sensitive = true }
+variable "db_password" { type = string sensitive = true }
+variable "db_name"     { type = string default   = "appdb" }
